5073d8fb0a2040aadbf6835f1ccb005a643fd8f9
k741030ken <k741030ken@gmail.com>
2017-08-27 15:01:21 +0800

    change Name

---
+++ CallWebService.Libray/Service/RESTfulWebServiceFactory.cs
@@ -0,0 +1,59 @@
+﻿
+using CallWebService.Libray.Service.Template;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallWebService.Libray.Service
+{
+    public class CallWebServiceFactory
+    {
+        #region "Local"
+        /// <summary>
+        /// 提供TemplateService
+        /// </summary>
+        private TemplateService _templateService = new TemplateService();
+
+        #endregion "Local"
+
+        #region "Constructor"
+
+        private static CallWebServiceFactory _instance;
+
+        /// <summary>
+        /// Private Constructor
+        /// </summary>
+        private CallWebServiceFactory()
+        {
+        }
+
+        [MethodImpl(MethodImplOptions.Synchronized)]
+        public static CallWebServiceFactory Instance()
+        {
+            if (_instance == null)
+            {
+                _instance = new CallWebServiceFactory();
+            }
+            return _instance;
+        }
+
+        #endregion "Constructor"
+
+        #region "Property"
+        /// <summary>
+        /// 取得目前的ItemService
+        /// </summary>
+        public ITemplateService templateService
+        {
+            get
+            {
+                return _templateService;
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ CallWebService.Libray/Service/Template/ITemplateService.cs
@@ -0,0 +1,17 @@
+﻿using CallWebService.Libray.DataBeans;
+using CallWebService.Libray.DataBeans.Template.Request;
+using CallWebService.Libray.DataBeans.Template.Response;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallWebService.Libray.Service.Template
+{
+    public interface ITemplateService
+    {
+
+        ReturnModel<List<BikeAvailabilityRespBean>> GetBikeAvailabilityData(UserReqBean user);
+    }
+}
